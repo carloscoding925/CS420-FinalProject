2d3e0f06e71ee8eeabbc57861dd1bd1342c6eda9
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-13 22:19:58 -0800

    padding

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,8 +6,8 @@ export default function Component() {
                     Welcome to the Web Developer Accessibility Tool!
                 </h1>
             </div>
-            <div className="flex flex-row place-content-center pt-4">
-                <video width="900" controls loop autoPlay muted>
+            <div className="flex flex-row place-content-center pt-4 pb-4">
+                <video width="800" controls loop autoPlay muted>
                     <source src="/demonstration.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
