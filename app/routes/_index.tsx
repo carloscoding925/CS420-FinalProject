d8be3edc663e50feb9028befb4f484ced6f55b4e
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-13 22:16:47 -0800

    Added video to home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,17 @@
 export default function Component() {
     return (
         <div>
-            Hello world!
+            <div className="flex flex-row place-content-center pt-4">
+                <h1 className="text-4xl font-bold">
+                    Welcome to the Web Developer Accessibility Tool!
+                </h1>
+            </div>
+            <div className="flex flex-row place-content-center pt-4">
+                <video width="900" controls loop autoPlay muted>
+                    <source src="/demonstration.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
         </div>
     );
 }
